bbbf23bbdd868c98c18b71a37412a1d36c45ab6d
Cristyan Giovane <cristyangiovane@hotmail.com>
2022-11-21 15:05:52 -0300

    Sync Setores

--- src/config/axios/ldap.ts
+++ src/config/axios/ldap.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const ldap = axios.create({
   baseURL: 'http://ldap.gruporisotolandia.com.br',
-  timeout: 3000,
+  timeout: 5000,
   headers: {
     token: 'ebc61675b2f5a553d340dacad99f35c6c3d188c7',
   },
--- src/modules/sync/controller/SyncController.ts
+++ src/modules/sync/controller/SyncController.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from 'express';
+import { ServiceSyncLocation } from '../services/ServiceSyncLocation';
 import { ServiceSyncSector } from '../services/ServiceSyncSector';
 
 export default class SyncController {
+  public async location(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
+    const svcSyncLocation = new ServiceSyncLocation();
+
+    const data = await svcSyncLocation.execute();
+    return response.json(data);
+  }
+
   public async sector(request: Request, response: Response): Promise<Response> {
     const svcSyncSector = new ServiceSyncSector();
 
