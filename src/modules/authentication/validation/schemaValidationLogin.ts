8d0c3983eac173cf9ca2c0f4f81b2e56b3e8ba47
Wellington Pika <wellingtonpiska@gmail.com>
2023-01-11 13:58:55 -0300

    att

--- src/modules/authentication/validation/schemaValidationLogin.ts
+++ src/modules/authentication/validation/schemaValidationLogin.ts
@@ -1,8 +1,10 @@
 import * as yup from 'yup';
 
 const schemaBodyAuthenticationLogin = yup.object().shape({
-  login: yup.string().required('Login is required'),
-  password: yup.string().required('Password is required'),
+  body: yup.object().shape({
+    login: yup.string().required('Login is required'),
+    password: yup.string().required('Password is required'),
+  }),
 });
 
 export default schemaBodyAuthenticationLogin;
