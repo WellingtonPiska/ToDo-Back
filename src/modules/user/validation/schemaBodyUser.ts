523501c49c75c564d3c18f0fbccd1455ccd43a3e
Wellington Pika <wellingtonpiska@gmail.com>
2023-01-11 13:39:30 -0300

    att

--- src/modules/user/validation/schemaBodyUser.ts
+++ src/modules/user/validation/schemaBodyUser.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 const schemaBodyUser = yup.object().shape({
   login: yup.string().required('Campo necessário!'),
   name: yup.string().required('Campo necessário!'),
-  lastname: yup.string().required('Campo necessário!'),
+  lastName: yup.string().required('Campo necessário!'),
   mail: yup.string().required('Campo necessário!'),
   phone: yup.string().notRequired(),
   avatar: yup.string().notRequired(),
